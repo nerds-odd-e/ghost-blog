27da52cfa55f926d991e233dfe28f056a6d37d2b
zbcjackson <zbcjackson@gmail.com>
2024-02-22 20:16:15 +0800

    :sparkles: Clean output dir first

--- src/main.js
+++ src/main.js
@@ -1,18 +1,28 @@
+import fs from "fs";
 import path from 'path';
 import {migratePosts} from "./posts.js";
 import {migrateImages} from "./images.js";
 import {createZipFile} from "./zip.js";
 
 export default function main() {
-
   const outputDir = 'output';
   const options = {
     outputDir: outputDir,
     contentDir: path.join(outputDir, 'blog'),
     backupDir: process.argv[2] || '/Users/zbcjackson/Downloads/Movable_Type-2024-01-19-13-49-15-Backup/',
   }
+  cleanOutputDir(options);
+
   migratePosts(options);
   migrateImages(options);
 
   createZipFile(options);
+}
+
+function cleanOutputDir(options) {
+  const outputDir = options.outputDir;
+  if (fs.existsSync(outputDir)) {
+    fs.rmSync(outputDir, { recursive: true });
+  }
+  fs.mkdirSync(outputDir);
 }
